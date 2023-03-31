9bdbb5d19c94b544dd4b6eacdf8c13be2cb954c8
Jean <64726220+jeanth20@users.noreply.github.com>
2023-03-31 07:57:37 +0200

    Add files via upload

---
+++ flaskfiles.py
@@ -0,0 +1,80 @@
+# server python file
+from flask import Flask, render_template, abort, send_file #, safe_join
+from werkzeug.utils import safe_join
+from pathlib import Path
+import os
+import datetime as dt
+
+
+# create a server instance
+app = Flask(__name__)
+
+FolderPath = os.getcwd()
+print(FolderPath)
+# FolderPath = r"/home/python/git/calendar/calendar-api-main"
+
+
+
+@app.route('/')
+def index():
+    return "Hello World!!!"
+
+def getReadableByteSize(num, suffix='B') -> str:
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Y', suffix)
+
+def getTimeStampString(tSec: float) -> str:
+    tObj = dt.datetime.fromtimestamp(tSec)
+    tStr = dt.datetime.strftime(tObj, '%Y-%m-%d %H:%M:%S')
+    return tStr
+
+def getIconClassForFilename(fName):
+    fileExt = Path(fName).suffix
+    fileExt = fileExt[1:] if fileExt.startswith(".") else fileExt
+    fileTypes = ["aac", "ai", "bmp", "cs", "css", "csv", "doc", "docx", "exe", "gif", "heic", "html", "java", "jpg", "js", "json", "jsx", "key", "m4p", "md", "mdx", "mov", "mp3",
+                 "mp4", "otf", "pdf", "php", "png", "pptx", "psd", "py", "raw", "rb", "sass", "scss", "sh", "sql", "svg", "tiff", "tsx", "ttf", "txt", "wav", "woff", "xlsx", "xml", "yml"]
+    fileIconClass = f"bi bi-filetype-{fileExt}" if fileExt in fileTypes else "bi bi-file-earmark"
+    return fileIconClass
+
+# route handler
+@app.route('/reports/', defaults={'reqPath': ''})
+@app.route('/reports/<path:reqPath>')
+def getFiles(reqPath):
+    # Join the base and the requested path
+    # could have done os.path.join, but safe_join ensures that files are not fetched from parent folders of the base folder
+    absPath = safe_join(FolderPath, reqPath)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(absPath):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(absPath):
+        return send_file(absPath)
+
+    # Show directory contents
+    def fObjFromScan(x):
+        fileStat = x.stat()
+        # return file information for rendering
+        return {'name': x.name,
+                'fIcon': "bi bi-folder-fill" if os.path.isdir(x.path) else getIconClassForFilename(x.name),
+                'relPath': os.path.relpath(x.path, FolderPath).replace("\\", "/"),
+                'mTime': getTimeStampString(fileStat.st_mtime),
+                'size': getReadableByteSize(fileStat.st_size)}
+    fileObjs = [fObjFromScan(x) for x in os.scandir(absPath)]
+    # get parent directory url
+    parentFolderPath = os.path.relpath(
+        Path(absPath).parents[0], FolderPath).replace("\\", "/")
+    return render_template('files.html.j2', data={'files': fileObjs,
+                                                 'parentFolder': parentFolderPath})
+
+    # template_path = os.path.join(app.root_path, 'templates', 'files.html.j2')
+    # return render_template(template_path)
+
+
+
+# run the server
+app.run(host="0.0.0.0", port=50100, debug=True)
